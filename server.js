c8175e4a969946ad2cd77b1e659bcd3ba2ac5858
Swiftmas <adhdhazard1@gmail.com>
2016-09-02 16:36:47 -0700

    update for npcs :) :) :)
    Adds collision to npc

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require("http");
 var url = require('url');
 var fs = require('fs');
 var io = require('socket.io');
+var collmap = require("./coll.json");
 
 var server = http.createServer(function(request, response){
     var path = url.parse(request.url).pathname;
@@ -65,10 +66,102 @@ var server = http.createServer(function(request, response){
 server.listen(8080);
 
 ///COOL STUFF ///////////////
-//var gamedata = {"players": {"p1":{"pos":"2.2", "state":"normal", "team": "red"}, "p2":{"pos":"2.3", "state":"normal", "team": "blue"}, "p3":{"pos":"2.4", "state":"normal", "team": "green"}}};
-var gamedata = {"players": {}, "attacks": {}};
+////VARS////
+var gamedata = {"players": {}, "attacks": {}, "enemies": {"enemy1": {"pos": "20.2", "team": "blue"}}};
+
+///// Functions ////////
+function getSurroundings(origin, dist) {
+	dist = parseInt(dist);
+	surroundings = ["none", dist];
+	orig = [origin.split(".")[0], origin.split(".")[1]];
+	gp = gamedata.players
+    for (var player in gamedata.players){
+    	if (gp.hasOwnProperty(player)) {
+    		ppos = [gp[player].pos.split(".")[0], gp[player].pos.split(".")[1]];
+    		if (ppos[0] > orig[0] - dist && ppos[0] < parseInt(orig[0]) + dist && ppos[1] > orig[1] - dist && ppos[1] < parseInt(orig[1]) + dist) {
+    			aa = Math.abs(ppos[0] - orig[0]);
+    			bb = Math.abs(ppos[1] - orig[1]);
+    			hypot = Math.sqrt( aa*aa + bb*bb );
+    			console.log(hypot);
+    			if (hypot < surroundings[1]){
+					surroundings = [player, hypot, ppos[0], ppos[1], orig[0], orig[1]];
+				} else if (hypot == surroundings[1] && Math.floor((Math.random()*2)) == 1) {
+					surroundings = [player, hypot, ppos[0], ppos[1], orig[0], orig[1]];
+				};
+    		};
+    	};
+    };
+    if (surroundings[0] == "none"){ surroundings = []; };
+    return surroundings;
+};
+
+function moveNpcTo(npc, tar, tarx, tary, npcx, npcy){
+	if (npcx == tarx){
+		var heading = 0;
+	} else if (npcy == tary) {
+		var heading = 1;
+	} else {
+		var heading = Math.floor((Math.random() * 2));
+	}
+	
+	if (heading == 1){
+		if (npcx > tarx) {
+			var newcoords = (parseInt(npcx)-1)+"."+npcy;
+			if (collmap[newcoords] == 0){
+			gamedata.enemies[npc].pos = newcoords;
+			};
+		} else if (npcx < tarx) {
+			var newcoords = (parseInt(npcx)+1)+"."+npcy;
+			if (collmap[newcoords] == 0){
+			gamedata.enemies[npc].pos = newcoords;
+			};
+		};
+	} else {
+		if (npcy > tary) {
+			var newcoords = npcx+"."+(parseInt(npcy)-1);
+			if (collmap[newcoords] == 0){
+			gamedata.enemies[npc].pos = newcoords;
+			};
+		} else if (npcy < tary) {
+			var newcoords = npcx+"."+(parseInt(npcy)+1);
+			if (collmap[newcoords] == 0){
+			gamedata.enemies[npc].pos = newcoords;
+			};
+		};
+	};
+};
+
+///// Controllers ///////
+function enemycontroller(){
+	ge = gamedata.enemies
+	for (var enemy in ge){
+		if (ge.hasOwnProperty(enemy)){
+			closetarget = getSurroundings(ge[enemy].pos, 10);
+			if (closetarget.length > 0 && closetarget[1] > 1){
+				moveNpcTo(enemy, closetarget[0], parseInt(closetarget[2]), parseInt(closetarget[3]), parseInt(closetarget[4]), parseInt(closetarget[5]));
+			};
+		};
+	};
+};
+
+//// INIT ///////
+
+
+
+//// MAIN UPDATE ///////////
+setInterval(function() {
+	enemycontroller();
+}, 600)
+
+
+///// LISTENERS ////// NEEDS CLEANED ////////////////
 var listener = io.listen(server);
 listener.sockets.on('connection', function(socket){
+
+////// INIT ////////////
+  socket.emit('collmap', collmap);
+
+
 
 ///This is basically the update function /////////
   setInterval(function(){
@@ -93,7 +186,7 @@ listener.sockets.on('connection', function(socket){
     };
   });
 
-// Listens for attacks //////
+// Listens for attacks ////// !!!!!! NEEDS FUNCTION OUSIDE OF LISTENER  !!!!!!!!///////////////////////////
   socket.on('attacks', function(data) {
     console.log(data[0] + " attacked");
     gamedata.attacks["a" + data[0]] = gamedata.players[data[0]].pos;
